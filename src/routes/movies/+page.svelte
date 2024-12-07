bbe9a1b80713b5e717ac90482960c70f9bc018dd
Okan Sönmez <okso2004@gmail.com>
2024-12-07 23:03:08 +0100

    FEAT: added searchbar for searching movies

--- src/routes/movies/+page.svelte
+++ src/routes/movies/+page.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
 	export let data: { movies: Array<{ id: string; Title: string; Year: string; Poster: string }> };
 	import { goto } from '$app/navigation';
+
+	export let query: string;
 </script>
 
 <main>
 	<h1 class="text-2xl font-bold mb-4 text-center">Movies</h1>
+	<div class="search-movies">
+		<input
+			class="searchbar"
+			placeholder="Suche nach Filmen..."
+			type="text"
+			bind:value={query}
+			on:change={() => goto(`?query=${query}`)}
+		/>
+		<button on:click={() => goto(`?query=${query}`)}>Suchen</button>
+	</div>
 	<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
 		{#each data.movies as movie}
 			<!-- svelte-ignore a11y-click-events-have-key-events -->
@@ -14,7 +26,7 @@
 				on:click={() => goto(`/movies/${movie.id}`)}
 			>
 				<img
-					src={movie.Poster !== 'N/A' ? movie.Poster : '/fallback-image.jpg'}
+					src={movie.Poster !== 'N/A' ? movie.Poster : 'default-fallback-image.png'}
 					alt={movie.Title}
 					class="w-full h-64 object-cover"
 				/>
@@ -30,6 +42,33 @@
 <style>
 	h1 {
 		text-align: center;
+	}
+	
+	.search-movies {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.search-movies button {
+		padding: 0.5rem 1rem;
+		background-color: #333;
+		color: white;
+		border: none;
+		border-radius: 0.375rem;
+		margin-left: 5px;
+	}
+
+	.search-movies button:hover {
+		background-color: #444;
+		cursor: pointer;
+	}
+
+	.search-movies input {
+		width: 100%;
+		padding: 0.5rem;
+		margin: 1rem 0;
+		margin-right: 5px;
 	}
 
 	.grid {
