942b21848c018fa9d22bbc330af81ef9b4e3df00
Okan Sönmez <okso2004@gmail.com>
2024-12-11 19:55:58 +0100

    fix: correct attribute names for vercel to deploy

--- src/routes/movies/+page.server.ts
+++ src/routes/movies/+page.server.ts
@@ -4,11 +4,11 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-	user: process.env.DB_USER,
-	host: process.env.DB_HOST,
-	database: process.env.DB_NAME,
-	password: process.env.DB_PASSWORD,
-	port: Number(process.env.DB_PORT),
+	user: process.env.PG_USER,
+	host: process.env.PG_HOST,
+	database: process.env.PG_NAME,
+	password: process.env.PG_PASSWORD,
+	port: Number(process.env.PG_PORT),
 	ssl: {
 		rejectUnauthorized: false
 	}
